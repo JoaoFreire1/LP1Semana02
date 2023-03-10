0287a55ae8c54ef2fa4e92bdefb367ce78f196b3
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-10 12:13:19 +0000

    Exercise 01, prefix

--- ManipulaVar/Program.cs
+++ ManipulaVar/Program.cs
@@ -7,6 +7,13 @@ namespace ManipulaVar
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int x = int.Parse( args );
+            foreach (string arg in args)
+            {
+                Console.WriteLine(x++);
+                Console.WriteLine(--x);
+            }
+           
         }
     }
 }

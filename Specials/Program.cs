ffe2f0b76c55d0ae84dddf24e9e6c4b1a52a8e70
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-10 13:58:09 +0000

    Exercise 06, max and min values

---
+++ Specials/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           sbyte sbMin = sbyte.MinValue;
+           Console.WriteLine(sbMin);
+
+           sbyte sbMax = sbyte.MaxValue;
+           Console.WriteLine(sbMax);
+
+           short shortMin = short.MinValue;
+           Console.WriteLine(shortMin);
+
+           short shortMax = short.MaxValue;
+           Console.WriteLine(shortMax);
+
+           int intMin = int.MinValue;
+           Console.WriteLine(intMin);
+
+           int intMax = int.MaxValue;
+           Console.WriteLine(intMax);
+
+           long longMin = long.MinValue;
+           Console.WriteLine(longMin);
+
+           long longMax = long.MaxValue;
+           Console.WriteLine(longMax);
+
+           byte byteMin = byte.MinValue;
+           Console.WriteLine(byteMin);
+
+           byte byteMax = byte.MaxValue;
+           Console.WriteLine(byteMax);
+
+           ushort uMin = ushort.MinValue;
+           Console.WriteLine(uMin);
+
+           ushort uMax = ushort.MaxValue;
+           Console.WriteLine(uMax);
+
+           uint uintMin = uint.MinValue;
+           Console.WriteLine(uintMin);
+
+           uint uintMax = uint.MaxValue;
+           Console.WriteLine(uintMax);
+
+           ulong ulongMin = ulong.MinValue;
+           Console.WriteLine(ulongMin);
+
+           ulong  ulongMax = ulong.MaxValue;
+           Console.WriteLine(ulongMax);
+
+           char charMin = char.MinValue;
+           Console.WriteLine(charMin);
+
+           char charMax = char.MaxValue;
+           Console.WriteLine(charMax);
+
+
+        }
+    }
+}

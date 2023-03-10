a85d47a054adabe9b8a7cd5a380340f7d9b6df4e
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-10 13:24:45 +0000

    Exercise 04, Ask for values

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,7 +6,15 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Write the height of a cylinder");
+            string a = Console.ReadLine();
+            double aa = double.Parse(a );
+            Console.WriteLine("Write the radius of a cylinder");
+            string r = Console.ReadLine();
+            double rr = double.Parse(r );
+            double Pi = 3.1415926;
+            Console.WriteLine(" V = "(Pi)*((rr)*(rr))*(aa));
+            Console.WriteLine(" S = " ( 2 * Pi * rr ) * ( r + aa ));
         }
     }
 }

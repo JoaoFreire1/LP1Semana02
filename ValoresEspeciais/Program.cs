1b9ac65eff9fa368c7a5749ff6cb5c6ca5ed65fd
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-27 17:57:16 +0000

    Types max and min

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,65 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            sbyte sbmax = sbyte.MaxValue;
+            Console.WriteLine(sbmax);
+            sbyte sbmin = sbyte.MinValue;
+            Console.WriteLine(sbmin);
+
+            short shmax = short.MaxValue;
+            Console.WriteLine(shmax);
+            short shmin = short.MinValue;
+            Console.WriteLine(shmin);
+            
+            int intmax = int.MaxValue;
+            Console.WriteLine(intmax);
+            int intmin = int.MinValue;
+            Console.WriteLine(intmin);
+
+            long longmax = long.MaxValue;
+            Console.WriteLine(longmax);
+            long longmin = long.MinValue;
+            Console.WriteLine(longmin);
+
+            byte bymax = byte.MaxValue;
+            Console.WriteLine(bymax);
+            byte bymin = byte.MinValue;
+            Console.WriteLine(bytemin);
+
+            ushort ushmax = ushort.MaxValue;
+            Console.WriteLine(ushmax);
+            ushort ushmin = ushort.MinValue;
+            Console.WriteLine(ushmin);
+
+            uint uintmax = uint.MaxValue;
+            Console.WriteLine(uintmax);
+            uint uintmin = uint.MinValue;
+            Console.WriteLine(uintmin);
+
+            ulong ulongmax = ulong.MaxValue;
+            Console.WriteLine(ulongmax);
+            ulong ulongmin = ulong.MinValue;
+            Console.WriteLine(ulongmin);
+
+            char charmax = char.MaxValue;
+            Console.WriteLine(charmax);
+            char charmin = char.MinValue;
+            Console.WriteLine(charmin);
+
+            float floatmax = float.MaxValue;
+            Console.WriteLine(floatmax);
+            float floatmin = float.MinValue;
+            Console.WriteLine(floatmin);
+
+            double doublemax = double.MaxValue;
+            Console.WriteLine(doublemax);
+            double doublemin = double.MinValue;
+            Console.WriteLine(doublemin);
+
+            decimal decimalmax = decimal.MaxValue;
+            Console.WriteLine(decimalmax);
+            decimal decimalmin = decimal.MinValue;
+            Console.WriteLine(decimalmin);
         }
     }
 }

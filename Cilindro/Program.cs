06354acf9c0cd58353c0aff9b0cdfb0154bcedc3
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-27 17:43:00 +0000

    Exercise 05

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -8,8 +8,16 @@ namespace Cilindro
         {
             Console.WriteLine("Diga a altura do cilindro");
             string ainput = Console.ReadLine();
+            double a = double.Parse(ainput);
             Console.WriteLine("Diga o raio do cilindro");
             string rinput = Console.ReadLine();
+            double r = double.Parse(rinput);
+            double V = Math.PI * Math.Pow(r, 2) * a;
+            double Sa = 2 * Math.PI * r * (r + a);
+            string V1 = $"{V:f3}";
+            Console.WriteLine(V1);
+            string Sa1 = $"{Sa:f3}";
+            Console.WriteLine(Sa1);
 
         }
     }

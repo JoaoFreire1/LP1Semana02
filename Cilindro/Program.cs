0fb18b30b97a3c802ffdd0241e8c054b3e02e1f7
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-27 17:15:48 +0000

    Ask for input

---
+++ Cilindro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Diga a altura do cilindro");
+            string ainput = Console.ReadLine();
+            Console.WriteLine("Diga o raio do cilindro");
+            string rinput = Console.ReadLine();
+
+        }
+    }
+}

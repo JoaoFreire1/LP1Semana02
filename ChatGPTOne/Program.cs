aec6cf544c3060670654eff12b3a35e4bb797e63
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-28 11:28:16 +0000

    ChatGPTOne questions

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,7 +6,29 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Ask me Something!");
+            string question = Console.ReadLine();
+
+            while (true)
+            {
+                if (question == "What's your name")
+                    Console.WriteLine("Ambrosio");
+                    break;
+
+                if (question == "How are you")
+                    Console.WriteLine("Terrible");
+                    break;
+
+                if (question == "Do you like Shrek")
+                    Console.WriteLine("Yes of course");
+                    break;
+
+                if (question == "Are you alive")
+                    Console.WriteLine("No I'm dead inside");
+                    break;
+            }
+
+            
         }
     }
 }

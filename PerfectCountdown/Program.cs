cd82e6c643e536a5710fabf303d8f76989f2e20b
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-28 14:15:18 +0000

    Loop cycle

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -6,21 +6,25 @@ namespace PerfectCountdown
     {
         private static void Main(string[] args)
         {
-            string start1 = Console.ReadLine();
-            int start = int.Parse(start1);
-            Console.WriteLine($"Start number: {start}");
-            string step1 = Console.ReadLine();
-            int step = int.Parse(step1);
-            Console.WriteLine($"Step number: {step}");
-
-            if (start < 1)
+            while (true)
             {
-                Console.WriteLine("Out-of-range start or step. Try again.")
-            }
+                string start1 = Console.ReadLine();
+                int start = int.Parse(start1);
+                Console.WriteLine($"Start number: {start}");
+                string step1 = Console.ReadLine();
+                int step = int.Parse(step1);
+                Console.WriteLine($"Step number: {step}");
 
-            if (step < 1)
-            {
-                Console.WriteLine("Out-of-range start or step. Try again.")
+                if (start < 1)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                }
+
+                if (step < 1)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                    continue;
+                }
             }
         }
     }

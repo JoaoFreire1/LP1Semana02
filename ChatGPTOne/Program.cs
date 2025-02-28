9ff38ffb974455909fb0e486599e9467ccb2f6ee
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-28 11:54:58 +0000

    Exercise 10 complete

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,25 +6,40 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            While (true)
+            while (true)
             {
                 Console.WriteLine("Please ask me a question!");
-                string question == Console.ReadLine();
+                string question = Console.ReadLine();
 
                 if (question == "How are you")
+                {
                     Console.WriteLine("Terrible");
-                    break;
+                    continue;
+                }
                 if (question == "What are you doing")
+                {
                     Console.WriteLine("Nothing");
-                    break;
+                    continue;
+                }
                 if (question == "Do you like cats")
+                {
                     Console.WriteLine("Yes");
-                    break;
+                    continue;
+                }
                 if (question == "Are you alive")
+                {
                     Console.WriteLine("No");
-                    break;
+                    continue;
+                }
                 if (question == "EXIT")
+                {
                     break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry I don't understand");
+                    continue;
+                }
             }
 
             
